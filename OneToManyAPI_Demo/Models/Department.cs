061a1be0ff0126e27dfd4839c80b3fc1c9ccd514
Varshitha.M <Varshitha.M@ENCDAPHYDLT0602>
2025-10-29 15:02:50 +0530

    Add project files.

---
+++ OneToManyAPI_Demo/Models/Department.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OneToManyAPI_Demo.Models
+{
+    public class Department
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+        public ICollection<Employee>? Employees { get; set; }
+    }
+}
---
+++ OneToManyAPI_Demo/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OneToManyAPI_Demo.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        [ForeignKey("Department")]
+        public int DepartmentId { get; set; }
+        public Department? Department { get; set; }
+    }
+}
